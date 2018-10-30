774cccd75f7d947f9262e0636a647705638b52e1
Wyk951117 <42219266+Wyk951117@users.noreply.github.com>
2018-10-30 16:11:18 -0400

    update client and temp_strategy_class

--- server-client demo/client.py
+++ server-client demo/client.py
@@ -1,11 +1,14 @@
 import asyncio
 import websockets
+import pickle
 
 async def talking():
 	async with websockets.connect(
+		#'ws://localhost:8765') as websocket:
 		'ws://45.76.164.162:10') as websocket:
 
-		file_name = 'test.py'
+		#file_name = input("Input file name:\n")
+		file_name = 'temp_strategy_class.py'
 
 		await websocket.send(file_name)
 		print(f">{file_name} sent")
@@ -20,6 +23,22 @@ async def talking():
 
 		feedback = await websocket.recv()
 		print(f"< {feedback}")
+
+		request = await websocket.recv()
+		print(f"< {request}")
+
+		titles = pickle.dumps(['USDT_BTC_10sec_s'])
+		await websocket.send(titles)
+
+		feedback_figure = open('feedback_figure.jpg','wb')
+		while True:
+			image_slice = await websocket.recv()
+			if image_slice == 'END OF FILE':
+				feedback_figure.close()
+				print("feedback figure received...")
+				break
+			else:
+				feedback_figure.write(image_slice)
 
 
 asyncio.get_event_loop().run_until_complete(talking())
---
+++ server-client demo/temp_strategy_class.py
@@ -0,0 +1,76 @@
+import random
+
+class temp_strategy_class():
+    """ a template for strategy class
+    This is a template for a strategy class that should be 
+    working well on backtesting.
+        
+    Attributes:
+        index_Df: including Up, Down and RSI
+        Up: measure of price increasing
+        Down: measure of price decreasing
+        gap: first direivative of price
+        data_Df: N/A
+
+    """
+
+    def __init__(self):
+        # create a dataframe for all the indexes you need for 
+        # computing and decision making
+        #self.index_Df = pd.DataFrame(index=[], columns=['Up','Down','RSI'])
+        self.gap = 0
+        self.Up = 0
+        self.Down = 0
+        self.RSI = 1
+
+    def compute(self,input_Df,input_window,input_label):
+        """ compute to decide action
+        compute using provided trading records and make
+        an action (buy/sell) based on the result.
+         
+        Args:
+            input_Df: the dataframe containing trading records
+                provided for your strategy, there might be one
+                or more data streams depending on you request;
+            input_window: the length of the sliding window of 
+                trading records. it is now fixed, but we'll see
+                whether it can be customized later;
+            input_label: buy/sell label, which is also based on
+                your request.
+
+        Returns:
+            action: a decision made based on the result of computing
+        """
+        for k in range(1,input_window-1):
+            self.gap =input_Df.loc[k+1][input_label] - input_Df.loc[k][input_label]
+            if self.gap > 0:
+                self.Up += self.gap
+            else:
+                self.Down += self.gap
+            if self.Up == 0 and self.Down == 0:
+                #print ('there is neither buy nor sell.')
+                return None
+            else:
+                self.RSI = self.Up/(self.Up + self.Down)
+                #print("RSI = {}".format(self.RSI))
+
+            # decision making process
+            core = random.random()
+            if self.RSI>0.8:
+                #print('make a sell order')
+                return 'buy'
+            else:
+                #print('make a buy order')
+                return 'sell'
+
+            restore_ratios()
+ 
+    def restore_ratios():
+        """
+        This is an example of how you could upgrade some of your
+        indexes during computing.
+        """
+        self.Up = 0
+        self.Down = 0
+        self.gap = 0
+        self.RSI =1
