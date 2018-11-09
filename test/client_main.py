e738c74967d6fd299a4783999ef614982c718bfb
Wyk951117 <42219266+Wyk951117@users.noreply.github.com>
2018-11-09 13:21:08 -0500

    update test folder

--- test/client_main.py
+++ test/client_main.py
@@ -1,15 +1,21 @@
 import asyncio
 import websockets
 import pickle
+
 from utils import talking
 
+strategy_dir = 'strategies/'                 # the directory to place strategy files
+config_dir = 'configs/'                      # the directory to place configuration files
+feedback_dir = 'feedback/'                     # the directory to place feedback figures
 
-strategy_file = ''        # the name of strategy file
-strategy_path = ''        # path where strategy file is 
-
-# TODO multiple config files are possible, figure it out
-config_file = ''          
-config_path = ''          # path where config files are
+strategy_name = 'temp_strategy_class.py'     # the name of your strategy file
+internal_config = 'internal_config.json'     # file contains parameters used to grab data from server
+external_config = 'external_config.json'     # file contains parameters for strategy
 
 
-asyncio.get_event_loop().run_until_complete(talking(strategy_file, strategy_path, config_file, config_path))
+
+
+
+asyncio.get_event_loop().run_until_complete(talking(strategy_dir, strategy_name,
+                                                    internal_config, external_config, 
+                                                    config_dir, feedback_dir))
--- test/utils.py
+++ test/utils.py
@@ -2,75 +2,65 @@ import asyncio
 import websockets
 import pickle
 
-# TODO get rid of trivial file names from this utils
-# and add handlers for file names include strategy and 
-# config files.
-async def talking(strategy_file, strategy_path, config_file, config_path):
+async def talking(strategy_dir, strategy_name, internal_config, external_config, config_dir, feedback_dir):
     async with websockets.connect(
+        #'ws://localhost:8765') as websocket:
         'ws://45.76.164.162:10') as websocket:
 
-        # send strategy file names to shake
-        # hands with server
-        file_name = 'temp_strategy_class.py'
-        await websocket.send(file_name)
-        print(f">{file_name} sent")
+        #file_name = input("Input file name:\n")
+        #file_name = 'temp_strategy_class.py'
 
-        # receive feedback from server
+        async def send_file(file_name):
+            # send file
+            with open(file_name, 'rb') as f:
+                for line in f:
+                    await websocket.send(line)
+                await websocket.send("END OF FILE")
+        # send strategy_name greeting
+        await websocket.send(strategy_name)
+        print(f">{strategy_name} sent")
+
+        # greeting from server
         greeting = await websocket.recv()
         print(f"< {greeting}")
 
-        """
-        with open(file_name, 'rb') as f:
-            for data in f:
-                await websocket.send(data)
-        await websocket.send('END OF FILE')
-        """
         # send strategy file
-        send_file(file_name)       
+        await send_file(strategy_dir + strategy_name)
+        print(f"{await websocket.recv()}")  # feedback from server
+        
+        # send internal config
+        await send_file(config_dir + internal_config)
+        print(f"{await websocket.recv()}")
 
-        # TODO send config files one by one
-        # and perhaps delete some greetings
+        # send external config
+        await send_file(config_dir + external_config)
+        print(f"{await websocket.recv()}")
 
-        feedback = await websocket.recv()
-        print(f"< {feedback}")
-
+        # handler is not useful anymore
+        """
         request = await websocket.recv()
         print(f"< {request}")
 
         titles = pickle.dumps(['USDT_BTC_10sec_s'])
         await websocket.send(titles)
-
         """
-        feedback_figure = open('feedback_figure.jpg','wb')
         while True:
-            image_slice = await websocket.recv()
-            if image_slice == 'END OF FILE':
-                feedback_figure.close()
-                print("feedback figure received...")
+            info = await websocket.recv()
+            if info == "image name":
+                img_name = await websocket.recv()
+                feedback_figure = open(feedback_dir+img_name, 'wb')
+                while True:
+                    image_slice = await websocket.recv()
+                    if image_slice == 'END OF FILE':
+                        feedback_figure.close()
+                        print("feedback figure received...")
+                        break
+                    else:
+                        feedback_figure.write(image_slice)
+            elif info == "END OF DIR":
                 break
-            else:
-                feedback_figure.write(image_slice)
-        """
-        # receive feedback figures from server
-        recv_figure("feedback_figure.jpg")
 
 
-
-async def send_file(file_name):
-    ## send either strategy or config 
-    ## files from local to server
-    with open(file_name,'rb') as f:
-        for data in f:
-            await websocket.send(data)
-    await websocket.send('END OF FILE')
-
-async def recv_figure(figure_name):
-    ## receive feedback figures from server
-    feedback_figure = open(figure_name,'wb')
-    while True:
-        image_slice = await websocket.recv()
-        if image_slice == "END OF FILE":
-            feedback_figure.close()
-            print("feedback figure received,,,")
-        else:
-            feedback_figure.write(image_slice)
+              
+   
+#asyncio.get_event_loop().run_until_complete(talking("temp_strategy_class.py","internal_config.json","external_config.json"))
